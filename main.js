5e36294e0e7f3c29d534549832e0e35337558bee
Tapiwanashe Chakurungama <chakurungamatapiwanashe@gmail.com>
2024-08-03 16:57:43 +0200

    hey

---
+++ main.js
@@ -0,0 +1,40 @@
+const images = [
+  "url(images/1-INSPIRE.png)",
+  "url(images/3-LEARNDEEPLY.png)",
+  "url(./../images/banner.jpeg)",
+  "url(./../images/header.jpg)",
+  // Add more image URLs as needed
+];
+const body = document.body
+const ham = document.querySelector(".hamburger")
+const links = document.querySelectorAll("a")
+
+let currentIndex = 0;
+
+function changeBackgroundImage() {
+  banner.style.backgroundImage = images[currentIndex];
+  currentIndex = (currentIndex + 1) % images.length;
+}
+
+setInterval(changeBackgroundImage, 5000); // Change image every 5 seconds
+
+// Initial call to set the first background image
+changeBackgroundImage();
+
+
+//hamburger and phone links showing
+
+
+ham.addEventListener("click",()=>{
+  body.classList.toggle("close")
+  body.classList.toggle("show")
+})
+
+// removing links show when links are clicked
+
+links.forEach(link =>{
+  link.addEventListener("click",()=>{
+    body.classList.remove("close");
+    body.classList.remove("show");
+  })
+})
